a51ad6a7a87cb10a240c988f69d755377898c737
Sara-Sofia <n2kesa00@students.oamk.fi>
2022-11-22 13:53:51 +0200

    harjotus3 tehty

--- harjotus03/harjotus03.js
+++ harjotus03/harjotus03.js
@@ -11,7 +11,13 @@ function calculate(e){
     let fData= new FormData(form);
     let num1=Number(fData.get("num1"));
     let num2=Number(fData.get("num2"));
-    let sum=num1+num2;
-    result.textContent="Tulos on"+" "+sum;
+    let arit=fData.get("arit");
+    let res;
+    if(arit=== "sum"){
+    res=num1+num2;
+    } else{
+    res=num1-num2;
+    }
+    result.textContent="Tulos on"+" "+res;
 
 }
